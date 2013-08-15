819335e3dc05f46a9abda8fd9de32a82227cab5e
thks10 <softinus@users.noreply.github.com>
2013-08-15 08:55:23 +0000

    -아이템 임시로 랜덤으로 나오게 함. -아이템 타입 데이터 설정 안되어 있던 문제 해결.

--- SpaceTrader2/src/com/raimsoft/spacetrader/scene/SGalaxyMap.java
+++ SpaceTrader2/src/com/raimsoft/spacetrader/scene/SGalaxyMap.java
@@ -99,6 +99,7 @@ public class SGalaxyMap extends SBase
 				userInfo.put(Global.PO_MOENY, 350);
 				userInfo.put(Global.PO_SHIP_TYPE, EnumShip.E_TRAINING_SHIP_1.ordinal());
 				userInfo.put(Global.PO_SHIP_HULL, 3500);
+				userInfo.put(Global.PO_SHIP_FUEL, 200);
 				userInfo.put(Global.PO_CROOD_WORLD_X, (int)tX);
 				userInfo.put(Global.PO_CROOD_WORLD_Y, (int)tY);
 				userInfo.put(Global.PO_CROOD_SYSTEM_MAP_PLANET, 1);
