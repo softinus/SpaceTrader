ffa33bc54f8b83fd755aa698b1f52400dd70bfeb
thks10 <softinus@users.noreply.github.com>
2013-12-15 12:39:46 +0000

    -parse setting. -UnityVS setting. -Create splash scene.

---
+++ SpaceTrader_Unity/Assets/ParseDataInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class ParseDataInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject["foo"] = "bar";
+        testObject.SaveAsync();
+        Debug.Log("Parse data init complete.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
