9bbd9b138abafabb509fa85a446c886c6cd17e69
thks10 <softinus@users.noreply.github.com>
2013-12-15 14:06:41 +0000

    sound, animation 추가.

--- SpaceTrader_Unity/Assets/ParseDataInput.cs
+++ SpaceTrader_Unity/Assets/ParseDataInput.cs
@@ -4,17 +4,29 @@ using Parse;
 
 public class ParseDataInput : MonoBehaviour {
 
+    private float fPastTime;
+    private Color currColor;
 	// Use this for initialization
 	void Start () 
     {
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject["foo"] = "bar";
-        testObject.SaveAsync();
+        //ParseObject testObject = new ParseObject("TestObject");
+        //testObject["foo"] = "bar";
+        //testObject.SaveAsync();
         Debug.Log("Parse data init complete.");
+
+        //currColor = this.gameObject.transform.renderer.material.color;
+        //currColor.r = 125;
+        //currColor.g = 125;
+        //currColor.b = 125;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        fPastTime += Time.deltaTime;
+        //Debug.Log(fPastTime.ToString());
+
+
+        
 	}
 }
